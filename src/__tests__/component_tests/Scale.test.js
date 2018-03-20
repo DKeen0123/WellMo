e96435295c198ef033f6711bde3fb3f0ccc225ca
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-20 12:43:54 +0000

    Test written, need to check test and code works

--- src/__tests__/component_tests/Scale.test.js
+++ src/__tests__/component_tests/Scale.test.js
@@ -8,4 +8,10 @@ describe('Scale', () => {
   it('renders correctly', () => {
     expect(scale).toMatchSnapshot();
   });
+
+  it('calls a function in the overall form component', () => {
+    const mockStateUpdate = jest.fn()
+    scale.find('#firstScale').simulate('change', {target: {name: 'moodScale', value: '99'}})
+    expect(mockStateUpdate).toHaveBeenCalled();
+  });
 });
--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Scale extends Component {
+
+  stateUpdate = () => console.log(1);
+
   render() {
     return (
       <div>
         <h1>Scale component</h1>
+      <input
+        id='firstScale'
+        onChange={this.stateUpdate}
+        type='number'
+        name ='moodScale'
+        min='0'
+        max='100'
+        />
       </div>
     );
   }
