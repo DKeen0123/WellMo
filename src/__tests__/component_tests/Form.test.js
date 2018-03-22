6ebae73d92c942c9720befbd95c026a86b4bdd22
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-22 14:57:50 +0000

    Closed testing gaps based on merge of 2 branches

--- src/__tests__/component_tests/Form.test.js
+++ src/__tests__/component_tests/Form.test.js
@@ -5,34 +5,52 @@ import Form from '../../components/Form';
 describe('Form', () => {
   let form = shallow(<Form />);
 
-  it('renders correctly', () => {
-    expect(form).toMatchSnapshot();
-  });
+  describe('Rendering components', () => {
+    it('renders correctly', () => {
+      expect(form).toMatchSnapshot();
+    });
 
-  it('renders a scale component', () => {
-    expect(form.find('Scale').exists()).toBe(true);
-  });
+    it('renders a scale component', () => {
+      expect(form.find('Scale').exists()).toBe(true);
+    });
 
-  it('renders a header component', () => {
-    expect(form.find('Header').exists()).toBe(true);
-  });
+    it('renders a header component', () => {
+      expect(form.find('Header').exists()).toBe(true);
+    });
 
-  it('renders an output component', () => {
-    expect(form.find('Output').exists()).toBe(true);
-  });
+    it('renders an output component', () => {
+      expect(form.find('Output').exists()).toBe(true);
+    });
 
-  it('passes updateState as props to scale Component', () => {
-    expect(form.find('Scale').prop('updateState')).toBe(
-      form.instance().updateState
-    );
-  });
+    it('renders a Question component', () => {
+      expect(form.find('Question').exists()).toBe(true);
+    });
 
-  it('passes savedFirstScaleScore as props to Output Component', () => {
-    form.setState({ savedFirstScaleScore: '99' });
-    expect(form.find('Output').prop('score')).toEqual(
-      form.state('savedFirstScaleScore')
-    );
-  });
+    it('renders an Submit component', () => {
+      expect(form.find('Submit').exists()).toBe(true);
+    });
+  })  
+
+  describe('Prop passing', () => {
+    it('passes updateState as props to scale Component', () => {
+      expect(form.find('Scale').prop('updateState')).toBe(
+        form.instance().updateState
+      );
+    });
+
+    it('passes displayScore as props to submit Component', () => {
+      expect(form.find('Submit').prop('displayScore')).toBe(
+        form.instance().displayScore
+      );
+    });
+
+    it('passes savedFirstScaleScore as props to Output Component', () => {
+      form.setState({ savedFirstScaleScore: '99' });
+      expect(form.find('Output').prop('score')).toEqual(
+        form.state('savedFirstScaleScore')
+      );
+    });
+  })
 
   describe('when Scale input is updated', () => {
     it('should update the state to the value of the input', () => {
