601d44eb103f3feefb15eae2ec2139a29d33ae8e
Joshua Holloway <josh.holloway@live.co.uk>
2018-03-22 16:00:12 +0000

    created more tests

--- src/__tests__/component_tests/Form.test.js
+++ src/__tests__/component_tests/Form.test.js
@@ -5,21 +5,22 @@ import Form from '../../components/Form';
 describe('Form', () => {
   let form = shallow(<Form />);
 
-  describe('Rendering components', () => {
-    it('renders correctly', () => {
-      expect(form).toMatchSnapshot();
-    });
+describe('Rendering components', () => {
+  it('renders correctly', () => {
+    expect(form).toMatchSnapshot();
+  });
 
+describe('button not clicked', () => {
     it('renders a scale component', () => {
       expect(form.find('Scale').exists()).toBe(true);
+     });
+
+    it('renders an output component', () => {
+      expect(form.find('Output').exists()).toBe(false);
     });
 
     it('renders a header component', () => {
       expect(form.find('Header').exists()).toBe(true);
-    });
-
-    it('renders an output component', () => {
-      expect(form.find('Output').exists()).toBe(true);
     });
 
     it('renders a Question component', () => {
@@ -29,7 +30,38 @@ describe('Form', () => {
     it('renders an Submit component', () => {
       expect(form.find('Submit').exists()).toBe(true);
     });
-  })  
+  });
+
+    describe('button is clicked', () => {
+        form.setState({buttonClicked: true});
+
+        it('renders an output component', () => {
+            expect(form.find('Output').exists()).toBe(true);
+        });
+
+        it('renders a scale component', () => {
+            expect(form.find('Scale').exists()).toBe(false);
+        });
+
+        it('renders a header component', () => {
+            expect(form.find('Header').exists()).toBe(true);
+        });
+
+        it('renders a Question component', () => {
+            expect(form.find('Question').exists()).toBe(true);
+        });
+
+        it('renders an Submit component', () => {
+            expect(form.find('Submit').exists()).toBe(false);
+        });
+  });
+
+
+
+
+
+
+
 
   describe('Prop passing', () => {
     it('passes updateState as props to scale Component', () => {
@@ -50,7 +82,7 @@ describe('Form', () => {
         form.state('savedFirstScaleScore')
       );
     });
-  })
+  });
 
   describe('when Scale input is updated', () => {
     it('should update the state to the value of the input', () => {
