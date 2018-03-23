f4cb0b009d9d35e67c1814725317c8360ec9384c
Joshua Holloway <josh.holloway@live.co.uk>
2018-03-23 11:01:18 +0000

    styling of background working but needs more work

---
+++ src/components/styledComponents/document.js
@@ -0,0 +1,10 @@
+// import styled from 'styled-components'
+// import weird_boat from './../../images/weird_boat.jpg'
+//
+// const Document = styled.div`
+//   background-image: url(${weird_boat});
+//   border-radius: 8px;
+//   color: white;
+// `;
+//
+// export default Document;
